9ff948035cd964226207c1b5aafe8cb7f69dbcc7
Hoang <48119382+viethoang303@users.noreply.github.com>
2023-09-12 17:18:16 +0700

    Update dist.py

--- ultralytics/utils/dist.py
+++ ultralytics/utils/dist.py
@@ -56,7 +56,7 @@ def generate_ddp_command(world_size, trainer):
     if not (safe_pattern.match(file) and Path(file).exists() and file.endswith('.py')):  # using CLI
         file = generate_ddp_file(trainer)
     # dist_cmd = 'torch.distributed.run' if TORCH_1_9 else 'torch.distributed.launch'
-    dist_cmd = 'torch.distributed.run' if TORCH_2_0 else 'torch.distributed.launch'
+    dist_cmd = 'torchrun' if TORCH_2_0 else 'torch.distributed.launch'
     port = find_free_network_port()
     cmd = [sys.executable, '-m', dist_cmd, '--nproc_per_node', f'{world_size}', '--master_port', f'{port}', file]
     return cmd, file
